7275bfb381f2d952239e9685be8d48cbcc385c1a
Jonatas Melo Silva <jonatas891024@gmail.com>
2023-11-28 14:58:48 -0300

    refactor: revisao dos estilos do layout public

--- src/layouts/Public.tsx
+++ src/layouts/Public.tsx
@@ -3,13 +3,14 @@ import { ClassNameValue, twMerge } from 'tailwind-merge'
 import { Templates } from '../templates'
 
 const base =
-  'flex min-h-screen w-full items-center flex-col bg-gray-100 antialiased' as ClassNameValue
+  'flex min-h-screen w-full flex-col items-center bg-gray-100 antialiased' as ClassNameValue
+const dark = 'dark:bg-neutral-950' as ClassNameValue
 
 export type PublicProps = ComponentProps<'div'>
 
 export function Public({ className, ...props }: PublicProps) {
   return (
-    <div className={twMerge(base, 'dark:bg-neutral-950', className)} {...props}>
+    <div className={twMerge(base, dark, className)} {...props}>
       <Templates.Header />
 
       <main className="flex h-full w-full flex-col gap-6 px-5 py-12 md:max-w-[1216px]">
