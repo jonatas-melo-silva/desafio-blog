5f16c8fb79339b64f3e83ef045afa83a0f10c271
Jonatas Melo Silva <jonatas891024@gmail.com>
2023-11-28 14:55:25 -0300

    refactor: revisao dos estilos do Card

--- src/components/Card/Data.tsx
+++ src/components/Card/Data.tsx
@@ -1,10 +1,12 @@
 import { ComponentProps } from 'react'
 import { ClassNameValue, twMerge } from 'tailwind-merge'
 
-const base = 'text-sm font-normal text-neutral-500' as ClassNameValue
+const base =
+  'text-sm font-normal text-neutral-500 md:text-base md:font-medium' as ClassNameValue
+const dark = 'dark:text-red-400' as ClassNameValue
 
 export type DataProps = ComponentProps<'p'>
 
 export function Data({ className, ...props }: DataProps) {
-  return <p className={twMerge(base, className)} {...props} />
+  return <p className={twMerge(base, dark, className)} {...props} />
 }
--- src/templates/Card.tsx
+++ src/templates/Card.tsx
@@ -11,7 +11,7 @@ import {
 } from '../components/Card'
 import { Post } from '../types/Post'
 
-const base = 'md:p-10' as ClassNameValue
+const base = '' as ClassNameValue
 
 export type CardProps = ComponentProps<'section'> & {
   post: Post
@@ -19,28 +19,15 @@ export type CardProps = ComponentProps<'section'> & {
 
 export function Card({ className, post, ...props }: CardProps) {
   return (
-    <Root
-      className={twMerge(
-        base,
-        'dark:border-2 dark:border-neutral-800 dark:bg-zinc-900 dark:hover:border-red-400 dark:active:border-red-400',
-        className,
-      )}
-      {...props}
-    >
+    <Root className={twMerge(base, className)} {...props}>
       <Wrapper>
-        <Data className="dark:text-red-400 md:text-base md:font-medium">
-          {post.date}
-        </Data>
+        <Data>{post.date}</Data>
         <Prefix>
-          <Heart className="h-5 w-5 text-indigo-600 dark:text-red-400 dark:active:fill-red-400 md:h-8 md:w-8" />
+          <Heart className="h-5 w-5 text-indigo-600 active:fill-indigo-600 dark:text-red-400 dark:active:fill-red-400 md:h-8 md:w-8" />
         </Prefix>
       </Wrapper>
-      <Title className="dark:text-white md:text-2xl md:font-medium">
-        {post.title}
-      </Title>
-      <Description className="dark:text-zinc-400 md:text-xl md:font-medium">
-        {post.content}
-      </Description>
+      <Title>{post.title}</Title>
+      <Description>{post.content}</Description>
     </Root>
   )
 }
