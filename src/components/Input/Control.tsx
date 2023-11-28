b03d6156ee96123e899d94929b32d4af44164d81
Jonatas Melo Silva <jonatas891024@gmail.com>
2023-11-28 14:41:57 -0300

    refactor: revisao dos estilos do compomente

--- src/components/Input/Control.tsx
+++ src/components/Input/Control.tsx
@@ -2,10 +2,11 @@ import { ComponentProps } from 'react'
 import { ClassNameValue, twMerge } from 'tailwind-merge'
 
 const base =
-  'flex w-full truncate bg-transparent p-0 text-sm font-medium text-white outline-none placeholder:text-white/50 placeholder:font-medium placeholder:text-sm' as ClassNameValue
+  'flex w-full truncate bg-transparent p-0 text-sm font-medium text-white outline-none placeholder:text-sm placeholder:font-medium placeholder:text-white/50 md:text-lg md:placeholder:text-lg' as ClassNameValue
+const dark = 'dark:placeholder-zinc-400' as ClassNameValue
 
 export type ControlProps = ComponentProps<'input'>
 
 export function Control({ className, ...props }: ControlProps) {
-  return <input className={twMerge(base, className)} {...props} />
+  return <input className={twMerge(base, dark, className)} {...props} />
 }
--- src/templates/Search.tsx
+++ src/templates/Search.tsx
@@ -3,8 +3,7 @@ import { ComponentProps } from 'react'
 import { ClassNameValue, twMerge } from 'tailwind-merge'
 import { Input } from '../components'
 
-const base =
-  'dark:border-2 dark:border-neutral-800 dark:bg-neutral-800/40 dark:focus-within:border-red-400 md:h-16 md:max-w-xs md:px-6 md:py-5' as ClassNameValue
+const base = '' as ClassNameValue
 
 export type SearchProps = ComponentProps<'input'>
 
@@ -15,7 +14,6 @@ export function Search({ className, ...props }: SearchProps) {
         <SearchIcon className="h-4 w-4 text-white dark:text-red-400 md:h-auto md:w-auto" />
       </Input.Prefix>
       <Input.Control
-        className="dark:placeholder-zinc-400 md:text-lg md:placeholder:text-lg"
         type="search"
         placeholder="Pesquisar no blog"
         name="search"
